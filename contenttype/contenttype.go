152ede00dd970ca52ffd037fcf2ab47eee33c029
Sufijen Bani <git@sbani.net>
2016-07-22 14:23:55 +0200

    WIP

--- contenttype/contenttype.go
+++ contenttype/contenttype.go
@@ -1,15 +1,15 @@
 package contenttype
 
+import "github.com/xeipuuv/gojsonschema"
+
 // ContentType explains the content type object
 type ContentType struct {
-	Key        string `storm:"id" json:"key" xml:"key" form:"key" valid:"required,alphanum"`
-	Validation string `json:"validation" xml:"validation" form:"validation" valid:"required,json"`
+	Key        string               `storm:"id" json:"key" xml:"key" form:"key" valid:"required,alphanum"`
+	Validation string               `json:"validation" xml:"validation" form:"validation" valid:"required,json"`
+	Schema     *gojsonschema.Schema `json:"omitempty"`
 }
 
-// NewContentType create a new content type
-func NewContentType(key, validation string) ContentType {
-	return ContentType{
-		Key:        key,
-		Validation: validation,
-	}
+// Cache stores ContentType in cache
+func (c *ContentType) Cache() {
+	Cache.Add(c)
 }
